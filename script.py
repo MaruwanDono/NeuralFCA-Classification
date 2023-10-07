086a1f18cf74f81b3e5af4f51022455d3bd4a287
MaruwanDono <marwan.bouabidi@hotmail.com>
2023-10-07 17:27:56 +0300

    Second update.

--- script.py
+++ script.py
@@ -10,16 +10,18 @@ from sklearn.metrics import accuracy_score, f1_score
 
 #Data
 datasets = ['ds_salaries', 'world_population', 'US_electricity_2017']
-ds_salaries_features = ['job_title', 'experience_level', 'salary_in_usd',
+ds_salaries_features = ['job_title', 'experience_level', 'salary_in_usd',\
     'work_year', 'employment_type', 'remote_ratio', 'company_size']
-world_population_features = ['Country/Territory', 'Rank', 'Continent',
-    'Growth Rate', '2022 Population', '2020 Population', '2015 Population',
+world_population_features = ['Country/Territory', 'Rank', 'Continent',\
+    'Growth Rate', '2022 Population', '2020 Population', '2015 Population',\
     '2010 Population', '2000 Population', 'Area (km²)','Density (per km²)']
-US_electricity_features = ['Utility.Number', 'Utility.Type',
-    'Demand.Summer Peak', 'Sources.Total', 'Uses.Total',
+US_electricity_features = ['Utility.Number', 'Utility.Type',\
+    'Demand.Summer Peak', 'Sources.Total', 'Uses.Total',\
     'Retail.Total.Customers']
-features = [ds_salaries_features, world_population_features, US_electricity_features]
-targets = [ds_salaries_features[2], world_population_features[3], US_electricity_features[2]]
+features = [ds_salaries_features, world_population_features,
+    US_electricity_features]
+targets = [ds_salaries_features[2], world_population_features[3],
+    US_electricity_features[2]]
 
 
 def readData(dataset_name, features):
